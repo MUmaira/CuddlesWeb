4ccb9ba082c752f4797b073f633296bc6fedfed7
CSSamarasinghe <it21227622@my.sliit.lk>
2023-10-23 04:05:01 +0530

    UIs Update

--- cuddles/src/pages/Account.jsx
+++ cuddles/src/pages/Account.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import "../styles/Account.css";
 
-const Account = () => {
+const Form = () => {
   return (
-    <div>
-      <h2>Account</h2>
-    </div>
-  )
-}
+    <form className="form">
+      <h2 style={{ marginBottom: "40px" }}> Personal Information </h2>
+      <div className="input-row">
+        <div className="input-container">
+          <p className="p">First Name</p>
+          <input
+            className="input"
+            type="text"
+            placeholder="First name"
+            name="firstName"
+          />
+        </div>
+        <div className="input-container">
+          <p className="p">Last Name</p>
+          <input
+            className="input"
+            type="text"
+            placeholder="Last name"
+            name="lastName"
+          />
+        </div>
+      </div>
+      <p className="p">Address</p>
+      <input
+        className="input"
+        type="text"
+        placeholder="Address"
+        name="address"
+      />
+      <p className="p">Email</p>
+      <input className="input" type="text" placeholder="Email" name="email" />
+      <div className="input-row">
+        <div className="input-container">
+          <p className="p">Contact Number</p>
+          <input
+            className="input"
+            type="text"
+            placeholder="Contact Number"
+            name="contactNumber"
+          />
+        </div>
+        <div className="input-container">
+          <p className="p">Designation</p>
+          <input
+            className="input"
+            type="text"
+            placeholder="Designation"
+            name="designation"
+          />
+        </div>
+      </div>
+      <div className="buttons-container">
+        <button className="upload-button" type="button">
+          Upload Photo
+        </button>
+        <button className="button" type="submit">
+          Confirm and Save
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default Account
+export default Form;
